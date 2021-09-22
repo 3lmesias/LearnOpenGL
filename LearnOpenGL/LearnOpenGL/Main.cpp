00accb2fff381a613f10869b5b06ed7d1589da26
Ramses <0194516@up.edu.mx>
2021-09-22 00:39:40 -0500

    [+]Hello triangle

--- LearnOpenGL/LearnOpenGL/Main.cpp
+++ LearnOpenGL/LearnOpenGL/Main.cpp
@@ -2,6 +2,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <fstream>
+#include <sstream> 
+#include <string> 
 #include "Shader.h"
 #include "VertexShader.h"
 #include "FragmentShader.h"
@@ -15,15 +18,19 @@ int Screen_W = 800;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+" vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
+"uniform vec4 ourColor;\n"
 "void main() {\n"
-"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"	FragColor = ourColor;\n"
 "}\0";
 
 
@@ -56,16 +63,49 @@ int main() {
 	//callabaks goes after the window is created and before the render loop
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+
+
+
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+
+
+
+
 	//triangle 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,
+		0.0f, 0.5f, 0.0f,    0.0f, 0.0f, 1.0f
 	};
+
+
+	float vertices2[] = {
+	0.5f, 0.5f, 0.0f, // top right
+	0.5f, -0.5f, 0.0f, // bottom right
+	-0.5f, -0.5f, 0.0f, // bottom left
+	-0.5f, 0.5f, 0.0f // top left
+	};
+
+	unsigned int indices[] = { // note that we start from 0!
+	0, 1, 3, // first triangle
+	1, 2, 3 // second triangle
+	};
+
+
+
+
 	// bind VAO
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
 	unsigned int VBO;
@@ -77,11 +117,15 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	//define attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 
 	//creta shader
@@ -100,17 +144,26 @@ int main() {
 	//	cout << "Error shader compilation error \n" << infoLog << endl;
 	//}
 
+	std::ifstream fsStream("Fragment Shader\\HelloTriangle._fs");
+	std::ifstream vsStream("Vertex Shader\\HelloTriangle._vs");
+	std::stringstream ssfs;
+	std::stringstream ssvs;
+	ssfs << fsStream.rdbuf();
+	ssvs << vsStream.rdbuf();
+	string auxFragment = ssfs.str();
+	string auxVertex = ssvs.str();
+
 	VertexShader vertexShader;
-	vertexShader.CretateShader(vertexShaderSource);
+	vertexShader.CretateShader(auxVertex.c_str());
 
 	FragmentShader fragmentShader;
-	fragmentShader.CretateShader(fragmentShaderSource);
+	fragmentShader.CretateShader(auxFragment.c_str());
 
 	if (!vertexShader.IsSuccessful()) {
-		cout << "Error shader compilation error \n" << vertexShader.GetError() << endl;
+		cout << "Error v shader compilation error \n" << vertexShader.GetError() << endl;
 	}
 	if (!fragmentShader.IsSuccessful()) {
-		cout << "Error shader compilation error \n" << fragmentShader.GetError() << endl;
+		cout << "Error f shader compilation error \n" << fragmentShader.GetError() << endl;
 	}
 	OGLProgram program;
 	program.AttachShaders(&fragmentShader, &vertexShader);
@@ -124,11 +177,21 @@ int main() {
 	vertexShader.Delete();
 	fragmentShader.Delete();
 
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
+
+
+
+
+	int vertexColorLocation = glGetUniformLocation(program.shaderProgram, "ourColor");
+	
 	//render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		float timeVAlue = glfwGetTime();
+		float greenValue = (sin(timeVAlue) / 2.0f) + 0.5f;
+
 		processInput(window);
 
 		//Configures color used by glClear
@@ -136,13 +199,19 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		program.Use();
+		//first use program before updating uniform
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
 		glBindVertexArray(VAO);
+
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//takes the render buffer and display it on the window
 		glfwSwapBuffers(window);
 		//check event triggers
 		glfwPollEvents();
+		glBindVertexArray(0);
 	}
 
 
